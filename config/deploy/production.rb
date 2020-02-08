49570b7fed869fb6b0d2049bb79199b1f926203b
iyra <iyra@nowhere>
2020-02-08 13:21:09 +0000

    initial

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,10 +3,10 @@ set :rails_env, "production"
 set :delayed_job_workers, 12
 append :linked_files, ".env.production"
 
-server "kagamihara", :roles => %w(web app cron), :primary => true
-server "shima", :roles => %w(web app)
-server "saitou", :roles => %w(web app)
-server "oogaki", :roles => %w(worker)
+server "kagamihara", :roles => %w(web app cron worker), :primary => true
+#server "shima", :roles => %w(web app)
+#server "saitou", :roles => %w(web app)
+#server "oogaki", :roles => %w(worker)
 
-set :newrelic_appname, "Danbooru"
+set :newrelic_appname, "Kurisuchan"
 after "deploy:finished", "newrelic:notice_deployment"
